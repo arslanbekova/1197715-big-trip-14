116b3eab403ff39e64f5a5b3dad411bd18b47b14
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-04-12 14:05:28 +0300

    Переведены простые компоненты в классы

--- src/view/cost-info.js
+++ src/view/cost-info.js
@@ -1,5 +1,29 @@
-export const createCostInfoTemplate = () => {
+import {createElement} from '../utils/general';
+
+const createCostInfoTemplate = () => {
   return `<p class="trip-info__cost">
     Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
   </p>`;
 };
+
+export default class CostInfo {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createCostInfoTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
