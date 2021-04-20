81da9dc6c0ac852bcea37ed557b42cced67f04cd
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-04-21 01:00:46 +0300

    Добавлена функция изменения флага isFavorite в презентер точки маршрута

--- src/presenter/route-point-presenter.js
+++ src/presenter/route-point-presenter.js
@@ -1,15 +1,17 @@
 import RoutePoint from '../view/route-point';
 import EditRoutePoint from '../view/edit-route-point';
-import {render, replace} from '../utils/render';
+import {render, replace, remove} from '../utils/render';
 
 export default class RoutePointPresenter {
-  constructor(eventsList) {
+  constructor(eventsList, addToFavorites) {
     this._eventsList = eventsList;
+    this._addToFavorites = addToFavorites;
 
     this._routePointComponent = null;
     this._editRoutePointComponent = null;
 
     this._handleRoutePointArrowClick = this._handleRoutePointArrowClick.bind(this);
+    this._handleFavoriteClick = this._handleFavoriteClick.bind(this);
     this._handleEditFormArrowClick = this._handleEditFormArrowClick.bind(this);
     this._handleEditFormSubmit = this._handleEditFormSubmit.bind(this);
     this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
@@ -18,14 +20,32 @@ export default class RoutePointPresenter {
   init(routePoint) {
     this._routePoint = routePoint;
 
+    const prevRoutePointComponent = this._routePointComponent;
+    const prevEditRoutePointComponent = this._editRoutePointComponent;
+
     this._routePointComponent = new RoutePoint(routePoint);
     this._editRoutePointComponent = new EditRoutePoint(routePoint);
 
     this._routePointComponent.setArrowClickHandler(this._handleRoutePointArrowClick);
+    this._routePointComponent.setFavoriteClickHandler(this._handleFavoriteClick);
     this._editRoutePointComponent.setArrowClickHandler(this._handleEditFormArrowClick);
     this._editRoutePointComponent.setFormSubmitHandler(this._handleEditFormSubmit);
 
-    render(this._eventsList, this._routePointComponent);
+    if (prevRoutePointComponent === null || prevEditRoutePointComponent === null) {
+      render(this._eventsList, this._routePointComponent);
+      return;
+    }
+
+    if (this._eventsList.contains(prevRoutePointComponent.getElement())) {
+      replace(this._routePointComponent, prevRoutePointComponent);
+    }
+
+    if (this._eventsList.contains(prevEditRoutePointComponent.getElement())) {
+      replace(this._editRoutePointComponent, prevEditRoutePointComponent);
+    }
+
+    remove(prevRoutePointComponent);
+    remove(prevEditRoutePointComponent);
   }
 
   _openEditRoutePointForm() {
@@ -55,5 +75,17 @@ export default class RoutePointPresenter {
 
   _handleEditFormSubmit() {
     this._closeEditRoutePointForm();
+  }
+
+  _handleFavoriteClick() {
+    this._addToFavorites(
+      Object.assign(
+        {},
+        this._routePoint,
+        {
+          isFavorite: !this._routePoint.isFavorite,
+        },
+      ),
+    );
   }
 }
