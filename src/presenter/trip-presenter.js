df54472d0abb6397c41e50a68a709f05c8b1edcb
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-04-21 00:55:50 +0300

    Добавлена функция обновления моков и конкретной точки маршрута

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -4,22 +4,31 @@ import TripInfo from '../view/trip-info';
 import SortOptions from '../view/sort-options';
 import RoutePointPresenter from './route-point-presenter';
 import {render} from '../utils/render';
+import {updateItem} from '../utils/general';
 import {RenderPosition} from '../utils/const';
 
 export default class Trip {
   constructor(tripEventsContainer, tripInfoContainer) {
     this._tripEventsContainer = tripEventsContainer;
     this._tripInfoContainer = tripInfoContainer;
+    this._routePointPresenter = {};
 
     this._tripInfoComponent = new TripInfo();
     this._costInfoComponent = new CostInfo();
     this._sortComponent = new SortOptions();
     this._noRoutePointsComponent = new NoRoutePoints();
+
+    this._handleAddToFavorites = this._handleAddToFavorites.bind(this);
   }
 
   init(routePoints) {
     this._routePoints = routePoints.slice();
     this._renderTrip();
+  }
+
+  _handleAddToFavorites(updatedRoutePoint) {
+    this.routePoints = updateItem(this._routePoints, updatedRoutePoint);
+    this._routePointPresenter[updatedRoutePoint.id].init(updatedRoutePoint);
   }
 
   _renderTripInfo() {
@@ -36,8 +45,9 @@ export default class Trip {
   }
 
   _renderRoutePoint(routePoint, eventsList) {
-    const routePointPresenter = new RoutePointPresenter(eventsList);
+    const routePointPresenter = new RoutePointPresenter(eventsList, this._handleAddToFavorites);
     routePointPresenter.init(routePoint);
+    this._routePointPresenter[routePoint.id] = routePointPresenter;
   }
 
   _renderRoutePoints() {
