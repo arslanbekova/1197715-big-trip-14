03ff2a3ba1c880feed6db532e899c7409ca0debb
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-05-16 22:37:16 +0300

    Изменена функция разметки фильтров

--- src/view/filter-options.js
+++ src/view/filter-options.js
@@ -1,22 +1,19 @@
 import Abstract from './abstract';
+import {FilterOption} from '../utils/const.js';
 
-const createFilterOptionsTemplate = () => {
+const createFilterOptionsTemplate = (currentFilterType = FilterOption.EVERYTHING) => {
   return `<form class="trip-filters" action="#" method="get">
-    <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-    </div>
-
-    <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-      <label class="trip-filters__filter-label" for="filter-future">Future</label>
-    </div>
-
-    <div class="trip-filters__filter">
-      <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-      <label class="trip-filters__filter-label" for="filter-past">Past</label>
-    </div>
-
+    ${Object.values(FilterOption).map((filterType) =>
+    `<div class="trip-filters__filter">
+      <input id="filter-${filterType}"
+        class="trip-filters__filter-input  visually-hidden"
+        type="radio"
+        name="trip-filter"
+        value="${filterType}"
+        ${currentFilterType === filterType ? 'checked' : ''}>
+      <label class="trip-filters__filter-label" for="filter-${filterType}">${filterType}</label>
+    </div>`,
+  ).join('')}
     <button class="visually-hidden" type="submit">Accept filter</button>
   </form>`;
 };
