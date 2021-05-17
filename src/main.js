76bfac82f5fd21d980db54045b0ccf4be60149d0
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-05-17 14:26:54 +0300

    Добавлен презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import TripPresenter from './presenter/trip-presenter';
+import FilterPresenter from './presenter/filter';
 import RoutePoints from './model/route-points';
 import Filter from './model/filter';
 import SiteMenu from './view/site-menu';
-import FilterOptions from './view/filter-options';
 import {generateRoutePoint} from './mock/route-point';
 import {render} from './utils/render';
 
@@ -24,12 +24,13 @@ const navigationContainer = tripInfoContainer.querySelector('.trip-controls__nav
 render(navigationContainer, new SiteMenu());
 
 const filterOptionsContainer = tripInfoContainer.querySelector('.trip-controls__filters');
-render(filterOptionsContainer, new FilterOptions());
+const filterPresenter = new FilterPresenter(filterOptionsContainer, filterModel, routePointsModel);
+filterPresenter.init();
 
 // Добавляет точки маршрута
 const siteMainElement = document.querySelector('.page-main');
 const tripEventsContainer = siteMainElement.querySelector('.trip-events');
 
-const tripPresenter = new TripPresenter(tripEventsContainer, tripInfoContainer, routePointsModel);
+const tripPresenter = new TripPresenter(tripEventsContainer, tripInfoContainer, routePointsModel, filterModel);
 tripPresenter.init();
 
