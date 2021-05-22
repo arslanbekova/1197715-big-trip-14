8a42314de59745e9f7a4f8859b2fac6d12ec8252
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-05-22 20:43:29 +0300

    Кнопка фильтра становится недоступной, если фильтру не соответствует ни одна из точек маршрута

--- src/view/filter-options.js
+++ src/view/filter-options.js
@@ -1,17 +1,19 @@
 import Abstract from './abstract';
-import {FilterOption} from '../utils/const.js';
+// import {FilterOption} from '../utils/const.js';
 
-const createFilterOptionsTemplate = (currentFilterType = FilterOption.EVERYTHING) => {
+const createFilterOptionsTemplate = (filters, currentFilterType) => {
+  // const {type, name, count} = filter;
   return `<form class="trip-filters" action="#" method="get">
-    ${Object.values(FilterOption).map((filterType) =>
+    ${filters.map((filter) =>
     `<div class="trip-filters__filter">
-      <input id="filter-${filterType}"
+      <input id="filter-${filter.type}"
         class="trip-filters__filter-input  visually-hidden"
         type="radio"
         name="trip-filter"
-        value="${filterType}"
-        ${currentFilterType === filterType ? 'checked' : ''}>
-      <label class="trip-filters__filter-label" for="filter-${filterType}">${filterType}</label>
+        value="${filter.type}"
+
+        ${currentFilterType === filter.type ? 'checked' : ''}>
+      <label class="trip-filters__filter-label" for="filter-${filter.type}">${filter.name}</label>
     </div>`,
   ).join('')}
     <button class="visually-hidden" type="submit">Accept filter</button>
@@ -19,15 +21,16 @@ const createFilterOptionsTemplate = (currentFilterType = FilterOption.EVERYTHING
 };
 
 export default class FilterOptions extends Abstract {
-  constructor(currentFilterType) {
+  constructor(filters, currentFilterType) {
     super();
+    this._filters = filters;
     this._currentFilterType = currentFilterType;
 
     this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createFilterOptionsTemplate(this._currentFilterType);
+    return createFilterOptionsTemplate(this._filters, this._currentFilterType);
   }
 
   _filterTypeChangeHandler(evt) {
