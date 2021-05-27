fc0c7ecabd2220f1ca1922a0a45ae3efa037cbf5
arslanbekova <lyuda.arslanbekova@gmail.com>
2021-05-27 11:06:12 +0300

    Изменен текст сообщения об ошибке

--- src/view/error.js
+++ src/view/error.js
@@ -2,7 +2,7 @@ import Abstract from './abstract.js';
 
 const createErrorTemplate = () => {
   return `<p class="trip-events__msg">
-    Sorry, something goes wrong...
+    Sorry, something went wrong...
     Please, try again later.
   </p>`;
 };
